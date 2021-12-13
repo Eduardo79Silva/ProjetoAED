14ff621d4c3d7b3805548ba315890a3eef0ab434
Naz <80106028+Eduardo79Silva@users.noreply.github.com>
2021-12-13 18:15:24 +0000

    Aeroporto updated

--- Aeroporto.h
+++ Aeroporto.h
@@ -5,12 +5,14 @@
 #ifndef PROJETOAED_AEROPORTO_H
 #define PROJETOAED_AEROPORTO_H
 #include "string"
-
+#include "RedeTransportes.h"
 
 class Aeroporto {
     std::string nome;
     std::string cidade;
     std::string pais;
+    RedeTransportes transportes;
+
 public:
     Aeroporto();
     Aeroporto(std::string n, std::string c, std::string p);
